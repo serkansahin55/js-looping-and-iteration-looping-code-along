fc142f7897d4d4ea055d2f04b3311135a85277a1
serkansahin55 <ssahin55@outlook.com>
2021-01-03 18:41:06 -0500

    commit tree 542c7afc74ed2d6db92e1e52985383f40a9ca5ac

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solutions in this file
+
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(array, event) {
+    let thankYouCards = []
+    for (let i = 0; i < array.length; i++) {
+        thankYouCards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+
+    return thankYouCards
+}
+
+
+function countDown(int) {
+    let i = 0
+    while (i < int ) {
+        console.log(int)
+        int -=1
+    }
+    console.log(int)
+}
+
